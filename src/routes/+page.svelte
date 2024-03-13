3e64973908ff0df272859be9e877183761746065
Alexander Doja <alexander.doja@outlook.com>
2024-03-13 14:58:53 +0100

    Home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
+<script>
+    import portrait from '../resources/portrait.jpg'
+</script>
 <main class="home-content">
-    <h1>Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <h1>Välkommen till min webbsida!</h1>
+    <br />
+    <img src={portrait} alt="En bild på Alexander Doja" />
+    <br />
+    <p>Hej, jag heter Alexander Doja. Jag är en student på utbildningen Systemutvecklare .NET med AI kompetens. Det här är min webbsida.</p>
 </main>
